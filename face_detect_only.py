74603c91b24e82f105c450ebc4fd198882c254b0
dan <phamvandan.cntt2@gmail.com>
2020-11-03 12:30:36 +0700

    change detection

--- face_detect_only.py
+++ face_detect_only.py
@@ -1,7 +1,25 @@
 import cv2
 import imutils
 import numpy as np
+
+def rotate_box(bbox, w, h, angle, ori_w, ori_h):
+    x1, y1, x2, y2, _ = bbox
+    if angle == 0:
+        boxes = np.asarray([x1*ori_w/w, y1*ori_h/h, x2*ori_w/w, y2*ori_h/h])
+        return boxes
+    elif angle == 90:
+        boxes = np.asarray([y1*ori_w/h, (w - x2)*ori_h/w, y2*ori_w/h, (w - x1)*ori_h/w])
+        return boxes
+    elif angle == 180:
+        boxes = np.asarray([(w - x2)*ori_w/w, (h - y2)*ori_h/h, (w - x1)*ori_w/w, (h - y1)*ori_h/h])
+        return boxes
+    elif angle == 270:
+        boxes = np.asarray([(h - y2)*ori_w/h, x1*ori_h/w, (h - y1)*ori_w/h, x2*ori_h/w])
+        return boxes
+
 def faceboxes_detect(image, face_model, img_heights, exact_thresh):
+    ori_h, ori_w = image.shape[:2]
+    origin_box = None
     box = None
     old_conf = 0.5
     image_rs = None
@@ -10,18 +28,20 @@ def faceboxes_detect(image, face_model, img_heights, exact_thresh):
         for i in range(4):
             if i != 0:
                 img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-                temp = np.float32(img)
-                bboxs = face_model.detect_faces(temp)
-                if bboxs is None or len(bboxs)==0:
-                    continue
-                bbox = bboxs[np.argmax(bboxs[:, 4])]
-                if bbox[-1] > old_conf:
-                    old_conf = bbox[-1]
-                    box = bbox
-                    image_rs = img
+            h, w = img.shape[:2]
+            temp = np.float32(img)
+            bboxs = face_model.detect_faces(temp)
+            if bboxs is None or len(bboxs)==0:
+                continue
+            bbox = bboxs[np.argmax(bboxs[:, 4])]
+            if bbox[-1] > old_conf:
+                old_conf = bbox[-1]
+                box = bbox
+                image_rs = img
+                origin_box = rotate_box(box, w, h, 90 * i, ori_w, ori_h)
         if old_conf > exact_thresh:
             break
-    return image_rs, box
+    return image_rs, box, origin_box
 
 if __name__ == '__main__':
     img_heights = [800, 1500]
--- main.py
+++ main.py
@@ -2,21 +2,29 @@
 import os
 import cv2
 import numpy as np
-import argparse
 import warnings
 import time
-
 from src.anti_spoof_predict import AntiSpoofPredict
 from src.generate_patches import CropImage
 from src.utility import parse_model_name
-warnings.filterwarnings('ignore')
+from moire import read_cfg, prepare_environment, fake_detection
+import pandas as pd
+from datetime import datetime
+from face_detect_only import faceboxes_detect
+import json
+import sys
 
+sys.path.insert(0, 'FaceBoxes.PyTorch/')
+import mytest
+
+warnings.filterwarnings('ignore')
 
 SAMPLE_IMAGE_PATH = ""
 
+
 def check_image(image, model_dir):
     height, width, channel = image.shape
-    if width/height != 3/4:
+    if width / height != 3 / 4:
         print("Image is not appropriate!!!\nHeight/Width should be 4/3.")
         return False
     else:
@@ -25,11 +33,11 @@ def check_image(image, model_dir):
 
 def dl_face_spoof_detect(image, model_dir, model_test, image_cropper, face_model, img_heights, exact_thresh):
     temp = image
-    image, image_bbox = faceboxes_detect(temp, face_model, img_heights, exact_thresh)
+    image, image_bbox, origin_bbox = faceboxes_detect(temp, face_model, img_heights, exact_thresh)
     if image is None:
-        image, image_bbox = model_test.get_bbox(temp)
-        if image is None:
-            return False, -1
+        # image, image_bbox = model_test.get_bbox(temp)
+        # if image is None:
+        return False, -1, origin_bbox
     prediction = np.zeros((1, 3))
     test_speed = 0
     # sum the prediction from single model's result
@@ -48,19 +56,20 @@ def dl_face_spoof_detect(image, model_dir, model_test, image_cropper, face_model
         img = image_cropper.crop(**param)
         start = time.time()
         prediction += model_test.predict(img, os.path.join(model_dir, model_name))
-        test_speed += time.time()-start
+        test_speed += time.time() - start
     print("Prediction cost {:.2f} s".format(test_speed))
     label = np.argmax(prediction)
     print(prediction)
     value = prediction[0][label] / 2
     if label == 1:
-        return False, value
-    return True, value
+        return False, value, origin_bbox
+    return True, value, origin_bbox
+
 
 def draw_prediction(image, image_bbox, prediction):
     # draw result of prediction
     label = np.argmax(prediction)
-    value = prediction[0][label]/2
+    value = prediction[0][label] / 2
     if label == 1:
         result_text = "RealFace Score: {:.2f}".format(value)
         color = (255, 0, 0)
@@ -76,17 +85,9 @@ def draw_prediction(image, image_bbox, prediction):
         image,
         result_text,
         (image_bbox[0], image_bbox[1] - 5),
-        cv2.FONT_HERSHEY_COMPLEX, 0.5*image.shape[0]/1024, color)
+        cv2.FONT_HERSHEY_COMPLEX, 0.5 * image.shape[0] / 1024, color)
     return image
 
-from  moire import read_cfg, prepare_environment, fake_detection
-import pandas as pd
-from datetime import datetime
-from face_detect_only import faceboxes_detect
-import json
-import sys
-sys.path.insert(0, 'FaceBoxes.PyTorch/')
-import mytest
 
 if __name__ == "__main__":
     # prepare environments
@@ -94,7 +95,7 @@ if __name__ == "__main__":
     # prepare parameters
     folder_int, folder_out, sigma_, sigmaMax, k, thresh, delta, device_id, model_dir, save_dir, img_heights, exact_thresh = read_cfg()
     img_heights = list(json.loads(img_heights))
-    ## load model
+    # load model
     face_model = mytest.face_boxes_model()
     face_model.load_face_model()
 
@@ -111,7 +112,11 @@ if __name__ == "__main__":
             print("can't read image")
             results.append([link_image, -1])
             continue
-        check_result, conf = dl_face_spoof_detect(img, model_dir, model_test, image_cropper, face_model, img_heights, exact_thresh)
+
+        # bbox can be None if detected fail
+        check_result, conf, bbox = dl_face_spoof_detect(img, model_dir, model_test, image_cropper, face_model,
+                                                        img_heights,
+                                                        exact_thresh)
         if check_result:
             print(link_image, "is fake with score=", conf)
             results.append([link_image, 1, conf])
@@ -123,4 +128,5 @@ if __name__ == "__main__":
             results.append([link_image, 0, conf])
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
-    pd.DataFrame(results, columns=["path_to_image", "is_fake", "conf"]).to_csv(os.path.join(save_dir, "{}_result.csv".format(datetime.now().strftime("%d_%m_%Y_%H_%M_%S"))))
+    pd.DataFrame(results, columns=["path_to_image", "is_fake", "conf"]).to_csv(
+        os.path.join(save_dir, "{}_result.csv".format(datetime.now().strftime("%d_%m_%Y_%H_%M_%S"))))
